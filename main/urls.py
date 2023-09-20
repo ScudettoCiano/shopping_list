4d927f406b27ebf0e86b342cf5ca31f8e1b32bb7
ScudettoCiano <scudetto024@gmail.com>
2023-09-20 15:59:31 +0700

    20/09

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,10 @@ from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
 # from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
+from main.views import login_user
+from main.views import logout_user
+
 app_name = 'main'
 
 urlpatterns = [
@@ -15,7 +19,9 @@ urlpatterns = [
     # path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
-
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
 
 # urlpatterns = [
