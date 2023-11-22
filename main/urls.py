6ca34a344acce0a98e9191af9a4acdc742b1226b
ScudettoCiano <scudetto024@gmail.com>
2023-11-22 15:35:34 +0700

    tutor 8

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user
 from main.views import logout_user
-from main.views import edit_product,get_product_json,add_product_ajax
+from main.views import edit_product,get_product_json,add_product_ajax,create_product_flutter
 from main.views import delete_product
 
 
@@ -29,7 +29,7 @@ urlpatterns = [
     path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
-
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 
 ]
 
